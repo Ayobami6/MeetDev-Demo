d2181b8c5aa6b4b906db25a2b6a3804f69ba3f90
Ayobami Alaran A. <ayobamidele006@gmail.com>
2023-09-15 11:17:41 +0000

    Add prettier formatter

--- server/index.ts
+++ server/index.ts
@@ -5,38 +5,36 @@ const app = express();
 
 const PORT: number = 3000;
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // get
 app.get('/', (req, res) => {
-    res.json({"Test": "Ok"})
-})
-
+  res.json({ Test: 'Ok' });
+});
 
 // test post
 app.post('/test', (req, res) => {
-    const requestBody: string = JSON.stringify(req.body)
-    res.send(`This is the post request body ${requestBody}`)
-})
+  const requestBody: string = JSON.stringify(req.body);
+  res.send(`This is the post request body ${requestBody}`);
+});
 
 // get dev
 app.get('/dev', (req, res) => {
-    const developer: dev = {
-        name: "Ayo",
-        role: "Backend"
-    }
-    res.json(developer);
-})
+  const developer: dev = {
+    name: 'Ayo',
+    role: 'Backend',
+  };
+  res.json(developer);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on Port ${PORT}`);
-})
+  console.log(`Server is running on Port ${PORT}`);
+});
 
 // add some interfaces
 
 interface dev {
-    name: string;
-    role: string;
+  name: string;
+  role: string;
 }
