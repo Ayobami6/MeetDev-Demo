e09af3d9249243e1a32f7f0a5c2fbf9d27a03ad7
Ayobami Alaran A. <ayobamidele006@gmail.com>
2023-09-15 11:10:32 +0000

    demo set up complete

---
+++ server/index.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+const PORT: number = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// get
+app.get('/', (req, res) => {
+    res.json({"Test": "Ok"})
+})
+
+
+// test post
+app.post('/test', (req, res) => {
+    const requestBody: string = JSON.stringify(req.body)
+    res.send(`This is the post request body ${requestBody}`)
+})
+
+// get dev
+app.get('/dev', (req, res) => {
+    const developer: dev = {
+        name: "Ayo",
+        role: "Backend"
+    }
+    res.json(developer);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`);
+})
+
+// add some interfaces
+
+interface dev {
+    name: string;
+    role: string;
+}
